241e4ebc08cde75602d49e7e803547bcfb9ae6f5
sararoseane <sararoseanesm@gmail.com>
2022-05-31 16:41:16 -0300

    ipfixo

--- catedral.py
+++ catedral.py
@@ -1,7 +1,11 @@
 from email import charset
+#importando firebird e pandas
 import firebirdsql
 import pandas as pd
+import matplotlib
 
+
+#conexão ao banco de dados
 con = firebirdsql.Connection(user='SYSDBA', 
 password='Laika06', 
 database='C:\\RDR\DADOS\\Catedral.FDB', 
@@ -16,7 +20,7 @@ for c in cur.fetchall():
     print(c)
 print('Finalizado')    """ 
 
-
+#query no banco de dados
 df = pd.read_sql_query("select * from sistb001_operador",con)
 print(df.head())
 
@@ -32,8 +36,18 @@ INNER JOIN sistb009_funcao fu
 ON f.nu_funcao = fu.nu_funcao;
 
 '''
+
 df = pd.read_sql_query (query, con)
 print(df.head())
 
-df = pd.read_sql("SISTB002_PARAMETRO",con)
-print(df.head())
+query_venda_funcionario = '''
+SELECT v.co_operador, count(*)FROM sistb007_venda v
+GROUP BY v.co_operador
+ORDER BY count(*) DESC;
+'''
+
+venda = pd.read_sql_query(query_venda_funcionario, con)
+print(venda.head())
+
+funcionarios_venda = venda.groupby('CO_OPERADOR')['COUNT'].mean()
+funcionarios_venda.head().plot.bar()
